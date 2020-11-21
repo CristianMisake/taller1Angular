f08d0cbbe709c9804bcf063abde2627089a8e815
Cristian Grondona <cristiangrondon@gmail.com>
2020-11-21 09:35:48 -0500

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+
+  employeeArray:Employee[] = [
+    { id:1, nombre:'Pedro Jose', pais:'Colombia' },
+    { id:2, nombre:'Maria Katalina', pais:'Venezuela' },
+    { id:3, nombre:'Margarita Madero', pais:'España' },
+  ]
+
+  seleccionEmpleado:Employee = new Employee();
 }
---
+++ src/app/models/employee.ts
@@ -0,0 +1,5 @@
+export class Employee {
+    id:number;
+    nombre:string;
+    pais:string
+}
